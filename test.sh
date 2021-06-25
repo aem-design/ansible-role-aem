28b45f65d4c319d3db19d4e4d5cb8429269a263d
wildone <max.barrass@gmail.com>
2021-06-25 12:14:06 +1000

    update role. [skip ci]

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ yum install -y python3
 pip3 install --ignore-installed PyYAML
 pip3 install --upgrade pip
 pip3 install testinfra molecule docker molecule-docker yamllint flake8 ansible
-molecule test
+molecule --debug test
